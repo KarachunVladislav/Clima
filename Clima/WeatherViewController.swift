3699169df5a77810ec58016a0640818f04b5c256
SpongeIsNotBoB <karachunvladislav@gmail.com>
2018-07-06 01:55:53 +0300

    completed clima

---
+++ Clima/WeatherViewController.swift
@@ -0,0 +1,117 @@
+import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
+
+
+class WeatherViewController: UIViewController, CLLocationManagerDelegate, ChangeCityDelegate {
+    
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    
+    let locationManager = CLLocationManager()
+    let weatherDataModel = WeatherDataModel()
+    
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+    locationManager.requestAlwaysAuthorization()
+    locationManager.startUpdatingLocation()
+        
+    }
+    
+    //MARK: - Networking
+    /***************************************************************/
+    func getWeatherData(url : String, parameters :[String : String] ){
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON { response in
+            if response.result.isSuccess{
+                print("Success!")
+                let weatherJSON : JSON = JSON(response.result.value!)
+                self.updateWeatherData(json: weatherJSON)
+                print(weatherJSON)
+            } else{
+                print("Error: \(response.result.error)")
+                self.cityLabel.text = "Connect Issue"
+            }
+        }
+    }
+
+    //MARK: - JSON Parsing
+    /***************************************************************/
+
+    func updateWeatherData(json: JSON){
+        if let tempResult = json["main"]["temp"].double {
+        
+        weatherDataModel.temperature = Int(tempResult - 273.15)
+        
+        weatherDataModel.city = json["name"].stringValue
+        
+        weatherDataModel.condition = json["weather"][0]["id"].intValue
+        
+        weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+            
+        updateUIWithWeatherData()
+        } else {
+            cityLabel.text = "Weather Unavaible"
+        }
+    }
+
+    //MARK: - UI Updates
+    /***************************************************************/
+
+    func updateUIWithWeatherData() {
+        cityLabel.text = weatherDataModel.city
+        temperatureLabel.text = "\(weatherDataModel.temperature)"
+        weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
+    }
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            let longitude = String(location.coordinate.longitude)
+            let latitude = String(location.coordinate.latitude)
+            
+            let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+            
+            getWeatherData(url: WEATHER_URL, parameters: params)
+            print("longitude = \(longitude), latitude = \(latitude)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
+
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+
+    func userEnteredANewCityName(city: String) {
+        print(city)
+        let params : [String : String] = ["q" : city, "appid" : APP_ID]
+        
+        getWeatherData(url: WEATHER_URL, parameters: params)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "changeCityName" {
+            let destinationVC = segue.destination as! ChangeCityViewController
+            destinationVC.delegate = self
+        }
+    }
+
+}
+
+
